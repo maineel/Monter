2cc6c8f1bf0f6970ba901668d8f959e218d2a5f8
neel <neelxdxd@gmail.com>
2024-04-18 19:19:25 +0530

    Minor fixes

--- src/db/index.js
+++ src/db/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async() => {
     try{
         const connnectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
+        console.log("MONGODB connection SUCCESSFUL");
     }catch(error){
         console.log("MONGODB connection FAILED: ",error);
         process.exit(1)
